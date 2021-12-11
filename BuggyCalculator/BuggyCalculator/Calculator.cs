2fc044aabc669742a14476c7ec4dd7d6d413406c
AlbinaShakirzyanova <butjafka@gmail.com>
2021-12-11 23:45:11 +0300

    Program code.

---
+++ BuggyCalculator/BuggyCalculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuggyCalculator
+{
+    public class Calculator
+    {
+        public double Div(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+
+        public int Multi(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Sub(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
---
+++ BuggyCalculator/BuggyCalculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BuggyCalculator
+{
+    class BuggyCalculator
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            string isContinue = "yes";
+
+            while (isContinue == "yes")
+            {
+                Console.WriteLine("Выберите операцию: +, -, *, /");
+                char operation = char.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите первое число:");
+                int number1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                int number2 = int.Parse(Console.ReadLine());
+
+                double result;
+
+                if (operation == '+')
+                {
+                    result = calculator.Sum(number1, number2);
+                }
+                else if (operation == '-')
+                {
+                    result = calculator.Sub(number1, number2);
+                }
+                else if (operation == '*')
+                {
+                    result = calculator.Multi(number1, number2);
+                }
+                else
+                {
+                    result = calculator.Div(number1, number2);
+                }
+
+                Console.WriteLine("Ваш результат: " + result);
+
+                Console.WriteLine("Продолжить?: yes/no");
+
+                isContinue = Console.ReadLine();
+            }
+        }
+    }
+}
